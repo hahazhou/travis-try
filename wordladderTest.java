f464be8816368b6f9d153a97154d1ad4dac3c8d6
hahazhou <haha-zhou@sjtu.edu.cn>
2018-05-18 21:44:15 +0800

    Add files via upload

---
+++ wordladderTest.java
@@ -0,0 +1,39 @@
+package word;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class wordladderTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindLadders() {
+		Findladders("hai","hei","")
+	}
+
+	@Test
+	void testGetNextWords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBfs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDfs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
